ae12c3f3d1a8fe6825f04a4786cd776c7a6b155e
Patrick Trasborg <patrick.trasborg@gmail.com>
2015-08-27 10:24:31 -0400

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,12 @@
+// http://slides.com/gtelljohann/reacto-selection-sort#
+//Create a selection sort algorithm
+
+//solution
+function selectionSort(array){
+	for(var i = 0; i < array.length; i++){
+		var minVal = array[i];
+		var minIdx = i;
+
+		for(var j = i + 1;)
+	}
+}
