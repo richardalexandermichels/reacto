7f327c2155dfb7ed7eb77e390bfbd9369fb797a7
Patrick Trasborg <patrick.trasborg@gmail.com>
2015-08-27 10:25:07 -0400

    selection sort implementation

--- selectionSort.js
+++ selectionSort.js
@@ -7,6 +7,23 @@ function selectionSort(array){
 		var minVal = array[i];
 		var minIdx = i;
 
-		for(var j = i + 1;)
+		for(var j = i + 1; j <array.length; j++){
+			if(array[j] < array[minIdx]){
+				minVal = array[j];
+				minIdx = j;
+			}
+		}
+
+		array[minIdx] = array[i];
+		array[i] = minVal;
 	}
+	return array;
 }
+
+//Performance
+//Time complexity:
+//O(n^2) for each item in the array, it takes time proportional to the number of items in the array
+
+//Space complexity:
+//O(n) total, O(1) extra - it's in place so you're only using constant space in addition to the initial array
+
